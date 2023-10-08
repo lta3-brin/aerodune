462241565ba265442f3643b1ec8e20d5cf88bbc2
akbarpn136 <akbarpn136@gmail.com>
2023-10-08 22:14:01 +0700

    Struktur awal aplikasi dengan leptos

---
+++ src/app.rs
@@ -0,0 +1,23 @@
+use leptos::*;
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "tauri"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+struct GreetArgs<'a> {
+    name: &'a str,
+}
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <main>
+            <h1>"Ini halaman utama AeroDune"</h1>
+        </main>
+    }
+}
