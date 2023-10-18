e48f012e98f4ac15945cf52494daa802c59dd6cf
akbarpn136 <akbarpn136@gmail.com>
2023-10-18 12:17:22 +0700

    Ganti posisi struktur tag

--- src/app/default.rs
+++ src/app/default.rs
@@ -9,30 +9,35 @@ use crate::stores::default::DefaultState;
 
 #[component]
 pub fn DefaultApp() -> impl IntoView {
-    provide_context(create_rw_signal(DefaultState::default()));
     provide_meta_context();
+    provide_context(create_rw_signal(DefaultState::default()));
 
     let state = expect_context::<RwSignal<DefaultState>>();
     let (light, _) = create_slice(state, |st| st.light, |st, val| st.light = val);
 
     view! {
         <Show when=light fallback=|| view! {
-            <Html lang="en" class="light" />
-        }>
             <Html lang="en" class="dark" />
+        }>
+            <Html lang="en" class="light" />
         </Show>
 
-        <div class="flex h-screen flex-col justify-between border-e bg-white">
-            <div class="px-4 py-6">
-                <DefaultBrand />
-                <DefaultSidebar />
+        <div class="grid grid-cols-1 lg:grid-cols-5">
+            <div class="flex h-screen flex-col justify-between border-e bg-white">
+                <div class="px-4 py-6">
+                    <DefaultBrand />
+                    <DefaultSidebar />
+                </div>
+
+                <div class="sticky inset-x-0 bottom-0 border-t border-gray-100">
+                    <DefaultSideFooter />
+                </div>
             </div>
 
-            <div class="sticky inset-x-0 bottom-0 border-t border-gray-100">
-                <DefaultSideFooter />
+            <div class="p-7 lg:col-span-4">
+                <HalamanUtama />
             </div>
         </div>
-
-        <HalamanUtama />
     }
 }
+
