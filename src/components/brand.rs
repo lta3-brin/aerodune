d25463e06e930ac709dd0c9ec0f3e4de4c21554a
akbarpn136 <akbarpn136@gmail.com>
2023-10-18 12:18:01 +0700

    Modifikasi struktur komponen

--- src/components/brand.rs
+++ src/components/brand.rs
@@ -3,9 +3,14 @@ use leptos::*;
 #[component]
 pub fn DefaultBrand() -> impl IntoView {
     view! {
-        <span class="grid h-10 w-32 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600">
-            Logo
-            <img src="public/logo.png" class="w-8 mr-2" alt="Logo aplikasi" />
-        </span>
+        <div class="flex place-items-center">
+            <div class="flex-none">
+                <img src="public/logo.png" class="w-8 mr-2" alt="Logo aplikasi" />
+            </div>
+            <div class="flex-1">
+                <h1 class="uppercase text-gray-700 font-bold">Dashboard</h1>
+            </div>
+        </div>
     }
 }
+
