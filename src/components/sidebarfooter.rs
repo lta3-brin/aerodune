237deab6b2a171c2e877820b283f79bd590da283
akbarpn136 <akbarpn136@gmail.com>
2023-10-18 12:18:50 +0700

    Penyederhanaan navigasi sidebar footer

--- src/components/sidebarfooter.rs
+++ src/components/sidebarfooter.rs
@@ -11,20 +11,9 @@ pub fn DefaultSideFooter() -> impl IntoView {
     };
 
     view! {
-        <a href="#" class="flex items-center gap-2 bg-white p-4 hover:bg-gray-50" on:click=onclick>
-            <img
-                alt="Man"
-                src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                class="h-10 w-10 rounded-full object-cover"
-            />
-
-            <div>
-                <p class="text-xs">
-                <strong class="block font-medium">Eric Frusciante</strong>
-
-                <span> eric@frusciante.com </span>
-                </p>
-            </div>
+        <a href="/" class="block border-y-gray-100 px-12 py-3 text-center text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700 focus:outline-none focus:ring active:bg-gray-100" on:click=onclick>
+            Tambah Kegiatan
         </a>
     }
 }
+
