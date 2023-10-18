0befd0997ca552d8773adeade93c9d2a6a979f12
akbarpn136 <akbarpn136@gmail.com>
2023-10-18 12:19:10 +0700

    Penyederhanaan template halaman utama

--- src/pages/utama.rs
+++ src/pages/utama.rs
@@ -7,11 +7,6 @@ pub fn HalamanUtama() -> impl IntoView {
         <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
         <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
         <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
     }
 }
+
