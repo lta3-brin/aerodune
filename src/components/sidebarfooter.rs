cec554c2fa75054e8028f9b71a03a75035257f26
userbaruu <61443015+userbaruu@users.noreply.github.com>
2023-10-18 09:35:15 +0700

    Modifikasi komponen footer bagian sidebar

--- src/components/sidebarfooter.rs
+++ src/components/sidebarfooter.rs
@@ -11,24 +11,20 @@ pub fn DefaultSideFooter() -> impl IntoView {
     };
 
     view! {
-        <section class="sidebar-footer justify-end bg-gray-2 pt-2">
-            <div class="divider my-0"></div>
-            <div class="dropdown z-50 flex h-fit w-full cursor-pointer hover:bg-gray-4">
-                <label class="whites mx-2 flex h-fit w-full cursor-pointer p-0 hover:bg-gray-4" tabindex="0">
-                    <div class="flex flex-row gap-4 p-4">
-                        <svg class="w-4 h-4 opacity-75" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" viewBox="0 0 20 20">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2"/>
-                        </svg>
-                        <div class="flex flex-col">
-                            <span>"Pengaturan"</span>
-                        </div>
-                    </div>
-                </label>
-                <div class="dropdown-menu-top-center md:dropdown-menu-right-top dropdown-menu ml-2">
-                    <a class="dropdown-item text-sm">"Tambah Kegiatan"</a>
-                    <a tabindex="-1" class="dropdown-item text-sm" on:click=onclick>"Ganti Tampilan"</a>
-                </div>
+        <a href="#" class="flex items-center gap-2 bg-white p-4 hover:bg-gray-50" on:click=onclick>
+            <img
+                alt="Man"
+                src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                class="h-10 w-10 rounded-full object-cover"
+            />
+
+            <div>
+                <p class="text-xs">
+                <strong class="block font-medium">Eric Frusciante</strong>
+
+                <span> eric@frusciante.com </span>
+                </p>
             </div>
-        </section>
+        </a>
     }
 }
