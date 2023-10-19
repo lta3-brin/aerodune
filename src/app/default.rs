83466e704b46b43d012df6b1a57f5a5fa3874c32
userbaruu <61443015+userbaruu@users.noreply.github.com>
2023-10-19 11:11:04 +0700

    Membuat menu interaksi tampil sidebar

--- src/app/default.rs
+++ src/app/default.rs
@@ -2,7 +2,8 @@ use leptos::*;
 use leptos_meta::*;
 
 use crate::components::{
-    brand::DefaultBrand, sidebar::DefaultSidebar, sidebarfooter::DefaultSideFooter,
+    buttongroups::DefaultBtns,
+    sidebar::{DefaultBrand, DefaultSideFooter, DefaultSideMenu, DefaultSidebar},
 };
 use crate::pages::utama::HalamanUtama;
 use crate::stores::default::DefaultState;
@@ -14,6 +15,12 @@ pub fn DefaultApp() -> impl IntoView {
 
     let state = expect_context::<RwSignal<DefaultState>>();
     let (light, _) = create_slice(state, |st| st.light, |st, val| st.light = val);
+    let (side, set_side) =
+        create_slice(state, |st| st.closesidebar, |st, val| st.closesidebar = val);
+
+    let onclicksidebar = move |_| {
+        set_side(true);
+    };
 
     view! {
         <Show when=light fallback=|| view! {
@@ -23,10 +30,10 @@ pub fn DefaultApp() -> impl IntoView {
         </Show>
 
         <div class="grid grid-cols-1 lg:grid-cols-5">
-            <div class="flex h-screen flex-col justify-between border-e bg-white">
+            <div class="hidden lg:flex h-screen flex-col justify-between border-e bg-white">
                 <div class="px-4 py-6">
                     <DefaultBrand />
-                    <DefaultSidebar />
+                    <DefaultSideMenu />
                 </div>
 
                 <div class="sticky inset-x-0 bottom-0 border-t border-gray-100">
@@ -35,9 +42,22 @@ pub fn DefaultApp() -> impl IntoView {
             </div>
 
             <div class="p-7 lg:col-span-4">
+                <div class="flex flex-row-reverse mb-3">
+                    <DefaultBtns />
+                </div>
                 <HalamanUtama />
             </div>
         </div>
+
+        <div class="absolute top-0 left-0 z-50 lg:hidden"
+            class:hidden=side
+        >
+            <DefaultSidebar />
+        </div>
+        <div
+            class="w-screen h-screen absolute top-0 left-0 z-20 bg-gray-700 opacity-70 lg:hidden"
+            class:hidden=side
+            on:click=onclicksidebar
+        />
     }
 }
-
