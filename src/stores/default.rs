c9541824b7808d1833cc34335d58a4da8f3c4813
userbaruu <61443015+userbaruu@users.noreply.github.com>
2023-10-19 11:12:51 +0700

    Tambah state untuk sidebar

--- src/stores/default.rs
+++ src/stores/default.rs
@@ -1,10 +1,14 @@
 #[derive(Debug, Clone)]
 pub struct DefaultState {
     pub light: bool,
+    pub closesidebar: bool,
 }
 
 impl Default for DefaultState {
     fn default() -> Self {
-        Self { light: true }
+        Self {
+            light: true,
+            closesidebar: true,
+        }
     }
 }
