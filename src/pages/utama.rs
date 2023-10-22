e62e4d776272fa892970fe972d7dd6b447523504
akbarpn136 <akbarpn136@gmail.com>
2023-10-22 21:14:09 +0700

    Modifikasi lanjut halaman utama

--- src/pages/utama.rs
+++ src/pages/utama.rs
@@ -3,10 +3,24 @@ use leptos::*;
 #[component]
 pub fn HalamanUtama() -> impl IntoView {
     view! {
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
-        <div class="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">+</div>
+        <section class="dark:bg-gray-800">
+            <div
+                class="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:items-center"
+            >
+                <div class="mx-auto max-w-xl text-center">
+                    <h1 class="text-3xl font-extrabold sm:text-5xl">
+                        <strong class="font-extrabold text-purple-700 sm:block">
+                            "Kalibrasi Instrumentasi"
+                        </strong>
+                        "LA3 BRIN"
+                    </h1>
+
+                    <p class="mt-4 sm:text-xl/relaxed">
+                        "Silahkan pilih kegiatan pada menu samping."
+                    </p>
+                </div>
+            </div>
+            </section>
     }
 }
 
