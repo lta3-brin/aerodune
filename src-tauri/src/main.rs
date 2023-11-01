a784858e0a4458692cb4dabb6886e911230a6b54
Fulan Gulan <fulan.gulan@gmail.com>
2023-11-01 11:46:10 +0700

    Modifikasi tauri dengan async

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,20 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod instruksi;
+mod model;
+mod store;
+
+use std::env;
+use surrealdb::engine::local::File;
+use surrealdb::Surreal;
+
 use crate::instruksi::kegiatan::greet;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let path = env::current_dir().unwrap();
+    let dir = format!("{}/data", path.display());
+    let _db = Surreal::new::<File>(dir).await.unwrap();
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
