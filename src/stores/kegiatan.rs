445845031aa0175fbd38abb3755e9162711b2c00
Fulan Gulan <fulan.gulan@gmail.com>
2023-11-08 13:00:07 +0700

    Menambahkan field page untuk state

--- src/stores/kegiatan.rs
+++ src/stores/kegiatan.rs
@@ -1,12 +1,14 @@
 #[derive(Debug, Clone)]
 pub struct KegiatanState {
     pub pesanalert: String,
+    pub kegiatanpage: u32,
 }
 
 impl Default for KegiatanState {
     fn default() -> Self {
         Self {
             pesanalert: "".to_string(),
+            kegiatanpage: 0,
         }
     }
 }
