8f8a07ba9abfc0487e9092b0decf5749a05c1471
akbarpn136 <akbarpn136@gmail.com>
2023-11-10 23:02:40 +0700

    Penyederhanaan fungsi main backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,25 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod instruksi;
-mod store;
-
-use anyhow::Result;
-use std::env;
-use surrealdb::engine::remote::ws::Ws;
-
-use crate::instruksi::kegiatan::tambahkegiatan;
-use crate::store::persistent::DB;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    DB.connect::<Ws>("localhost:8000").await?;
-    DB.use_ns("aerodune").use_db("kalibrasi").await?;
-
+fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![tambahkegiatan])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-
-    Ok(())
 }
+
