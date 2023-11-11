e53b71805ef68fe13ffabead7012985cfda00725
akbarpn136 <akbarpn136@gmail.com>
2023-11-11 08:08:44 +0700

    Ganti return type

--- src/controllers/kegiatan.rs
+++ src/controllers/kegiatan.rs
@@ -2,11 +2,11 @@ use anyhow::Result;
 use chrono::prelude::Local;
 
 use crate::configs::db::DB;
-use crate::models::kegiatan::KegiatanTambah;
+use crate::models::{Record, kegiatan::KegiatanTambah};
 
 impl KegiatanTambah {
-    pub async fn tambah(name: String) -> Result<Vec<Self>> {
-        let created: Vec<Self> = DB
+    pub async fn tambah(name: String) -> Result<Vec<Record>> {
+        let created: Vec<Record> = DB
             .create("kegiatan")
             .content(Self {
                 name,
