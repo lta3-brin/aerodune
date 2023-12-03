3dd42a3a1ba914d0574e469bd277213982f4ecb1
akbarpn136 <akbarpn136@gmail.com>
2023-12-03 13:07:02 +0700

    Membuatfungsi init db

--- src/controllers/kegiatan.rs
+++ src/controllers/kegiatan.rs
@@ -8,6 +8,20 @@ use crate::models::kegiatan::KegiatanMuat;
 use crate::models::{kegiatan::KegiatanTambah, Record};
 
 impl KegiatanTambah {
+    pub async fn init() -> Result<Vec<KegiatanMuat>> {
+        DB.connect::<Ws>("localhost:8000").await?;
+        DB.use_ns("aerodune").use_db("kalibrasi").await?;
+
+        let mut q = DB
+            .query("select * from kegiatan order by create desc limit $limit")
+            .bind(("limit", PAGE_LIMIT))
+            .await?;
+
+        let kegiatan: Vec<KegiatanMuat> = q.take(0)?;
+
+        Ok(kegiatan)
+    }
+
     pub async fn tambah(name: String) -> Result<Vec<Record>> {
         let created: Vec<Record> = DB
             .create("kegiatan")
@@ -21,10 +35,7 @@ impl KegiatanTambah {
         Ok(created)
     }
 
-    pub async fn muat(page: u32) -> Result<Vec<KegiatanMuat>> {
-        DB.connect::<Ws>("localhost:8000").await?;
-        DB.use_ns("aerodune").use_db("kalibrasi").await?;
-
+    pub async fn _muat(page: u32) -> Result<Vec<KegiatanMuat>> {
         let offset = PAGE_LIMIT * (page - 1);
         let mut q = DB
             .query("select * from kegiatan order by create desc limit $limit start $start")
